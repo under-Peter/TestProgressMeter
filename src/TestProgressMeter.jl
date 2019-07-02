04da494b1ba3c07d2bcd1e116a1d572ff107e6da
Andreas Peter <andreas.peter.ch@gmail.com>
2019-07-02 09:46:51 +0200

    Update insertProgress

--- src/TestProgressMeter.jl
+++ src/TestProgressMeter.jl
@@ -3,23 +3,34 @@ module TestProgressMeter
 export insertProgress, removeProgress
 
 @doc raw"
-    insertProgress(f = 'runtests.jl'; toplevel = true, s = 1)
-assuming `f` is a `runtest` folder, `insertProgress`
-inserts the necessary statements into `f` such that a progress-bar
-is presented during testing which is updated all `s` seconds.
-The progress-bar gives equal weight to each `@test...`- statement
-except `@testset`.
+    insertProgress(f = 'runtests.jl'; toplevel = true, ntests = nothing, loops = true, pmargs = ())
+recursively inserts `next!` statements before each test that's included in `f`
+and initializes the `ProgressMeter` at the beginning of the file according to the
+following rules:
+- if `ntests` - the number of tests - is provided, a progressbar will be shown with equal weight to each of the `ntests` tests
+- if `ntests` is not provided but `loops=false`, all `@test` will be counted and a progressbar will be shown with equal weight to each test
+- if neither `ntests` is provided nor `loops=true`, a simple counter will be shown of how many tests have been run
 
-To undo, apply `removeProgress(f)`.
+via `pmargs`, keyword-arguments to the `ProgressMeter`-object can be provided such as e.g.
+```julia
+julia> insertProgress(\"example/runtest.jl\", pmargs = (desc = \"Text next to errorbar\", dt = 0.1), ntests = 10)
+```
+to get a progressbar that updates up to ever `0.1` seconds and has the text in `desc` next to it.
 "
-function insertProgress(f = "runtests.jl"; toplevel = true, s = 1)
+function insertProgress(f = "runtests.jl"; toplevel = true,
+                                            ntests = nothing,
+                                            loops = true,
+                                            pmargs = ())
+    counter = 0
     isfile(f) || throw(ArgumentError("$f is not a file"))
 
     lines = readlines(f)
 
+    # if ProgressMeter is already used, assume its from TestProgressMeter and strip
+    # all lines that were inserted
     if toplevel && any(x -> occursin("using ProgressMeter", x), lines)
         removeProgress(f)
-        insertProgress(f, toplevel = toplevel, s = s)
+        insertProgress(f, toplevel = toplevel, ntests = ntests, loops = loops, pmargs = pmargs)
         return nothing
     end
 
@@ -27,23 +38,41 @@ function insertProgress(f = "runtests.jl"; toplevel = true, s = 1)
         m = match(r"^([ \t]*)@test[^s]", lines[i])
         if m != nothing
             lines[i] = string(m.captures[],"next!(pmobj)\n", lines[i])
+            counter += 1
         end
-        m = match(r"(include\(\")([^\")]*)(\"\))", lines[i])
+        m = match(r"^[^\#]*(include\(\")([^\")]*)(\"\))", lines[i])
         if m != nothing
-            dir = dirname(f)
-            nf  = joinpath(dir, m.captures[2])
-            insertProgress(nf; toplevel = false)
+            nf  = joinpath(dirname(f), m.captures[2])
+            counter += insertProgress(nf; toplevel = false)
         end
     end
-    toplevel && pushfirst!(lines, "using ProgressMeter", "pmobj = ProgressUnknown($s)")
-    toplevel && push!(lines,"finish!(pmobj)")
-    toplevel && push!(lines,"println()")
+    if toplevel
+        if length(pmargs) == 1
+            pmargsstring = chop(string(pmargs), head=1, tail=2)
+        else
+            pmargsstring = chop(string(pmargs), head=1, tail=1)
+        end
+        if ntests isa Integer
+            counter = ntests
+            pushfirst!(lines,
+                "pmobj = Progress($counter, $(pmargsstring))")
+        elseif loops == false
+            pushfirst!(lines,
+             "pmobj = Progress($counter, $(pmargsstring))")
+        else
+            pushfirst!(lines,
+             "pmobj = ProgressUnknown($(pmargsstring))")
+            lastend = findlast(x -> occursin("end", x), lines)
+            insert!(lines, lastend, "finish!(pmobj)")
+        end
+        pushfirst!(lines, "using ProgressMeter")
+        push!(lines, "println()")
+    end
 
     open(f, "w") do io
         write(io, join(lines,'\n'))
     end
-    return nothing
-
+    return counter
 end
 
 @doc raw"
@@ -69,6 +98,7 @@ function removeProgress(f = "runtests.jl")
             removeProgress(nf)
         end
     end
+    lines[end] == "println()" && (toremove[end] = true)
 
     deleteat!(lines, toremove)
     open(f, "w") do io
@@ -76,5 +106,6 @@ function removeProgress(f = "runtests.jl")
     end
     return nothing
 end
+
 
 end # module
